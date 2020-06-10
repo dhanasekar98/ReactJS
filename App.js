4b855a8763080a3f2cdc7217f0b97fef45f0fc17
dhanasekar98 <37210654+dhanasekar98@users.noreply.github.com>
2020-06-10 17:22:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React,{ Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import SignUp from './Component/SignUp'
+import Label from './Component/Label'
+
+
+
+
+
+class App extends Component{
+  render() {
+  return (
+    <div className="App">
+      <SignUp/>
+      
+      <Label/>
+      
+
+
+
+    
+
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ SignUp.js
@@ -0,0 +1,38 @@
+import React from "react"
+import './name.css'
+ 
+
+const App=() =>{
+
+
+
+    return(
+        <div>
+            <header className="sam">
+                <h1 className="SignUp">Sign Up <img src="images/flower.png" alt="" align="right"/></h1>
+                <div className="or">or 
+                <h5 className="login">login the account</h5></div>
+                
+
+        
+
+
+
+            </header>
+
+
+
+
+
+
+
+
+        </div>
+        
+
+
+
+    );
+};
+
+export default App
